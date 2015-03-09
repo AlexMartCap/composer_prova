e7da8284c9dbda8ae322bcf0c75f207aced91171
Alexandre <alexmart4@gmail.com>
2015-03-09 19:19:19 +0100

    second commit

--- holamon.php
+++ holamon.php
@@ -1,5 +1,11 @@
 <?php
-require "./vendor/illuminate/filesystem/Filesystem.php";
+
+use Illuminate\Filesystem\Filesystem;
+
+require "./vendor/autoload.php";
+
+//require "./vendor/illuminate/filesystem/Filesystem.php";
 
 $fs = new Filesystem();
+
 echo "Hola mon!";
